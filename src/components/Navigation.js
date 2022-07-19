0f91de588eaad8bb90849e129fb8eebd52ef723b
Owoeye1321 <owoeye1321@gmail.com>
2022-07-19 14:40:19 -0700

    new commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href="/home">
+                        Home 
+                    </a>
+                </li>
+                <li>
+                    <Link to="/service">Service</Link>
+                </li>
+                <li>
+                    <a href="#">
+                        Pages <i className="fal fa-angle-down" />
+                    </a>
+                    <ul className="sub-menu">
+                        <li>
+                            <Link to="/about-us">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/error">Error</Link>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">
+                        News <i className="fal fa-angle-down" />
+                    </a>
+                    <ul className="sub-menu">
+                        <li>
+                            <Link to="/news">News Page</Link>
+                        </li>
+                        <li>
+                            <Link to="/news/single-news">Single News</Link>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default Navigation;
